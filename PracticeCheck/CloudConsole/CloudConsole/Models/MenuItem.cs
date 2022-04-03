a77a99206782a32260de45175e5a90e6b77a0249
Arun <arun.reddy6541@gmail.com>
2022-04-03 19:16:57 +0530

    added

---
+++ PracticeCheck/CloudConsole/CloudConsole/Models/MenuItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CloudConsole.Models
+{
+    public class MenuItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Free Delivery")]
+        public bool FreeDelivery { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DateOfLaunch { get; set; }
+
+        public bool Active { get; set; }
+    }
+}
---
+++ PracticeCheck/CloudConsole/CloudConsole/Program.cs
@@ -0,0 +1,75 @@
+﻿using CloudConsole.Models;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Http;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+
+namespace CloudConsole
+{
+    public class Program
+    {
+        private string GenerateJSONWebToken()
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("mysuperdupersecret"));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+
+
+            var token = new JwtSecurityToken(
+                        issuer: "mySystem",
+                        audience: "myUsers",
+                        expires: DateTime.Now.AddMinutes(10),
+                        signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        static void Main(string[] args)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:15338/api/");
+                var response = client.GetAsync("MenuItem");
+                response.Wait();
+                var res = response.Result;
+                var list = res.Content.ReadAsAsync<MenuItem[]>();
+                list.Wait();
+                var menuItem = list.Result;
+                Console.WriteLine("Name\t\tPrice\tDeliveryStatus\tLaunchedDate\tActive");
+                foreach(var Item in menuItem)
+                {
+                    Console.WriteLine(Item.Name+"\t\t"+Item.Price+"\t"+Item.FreeDelivery+"\t\t"+Item.DateOfLaunch.ToShortDateString()+"\t"+Item.Active);
+                }
+            }
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:23966/api/");
+                Console.WriteLine("Enter a menuItemId");
+                int menuItemId = int.Parse(Console.ReadLine());
+                /*if(menuItemId<1 || menuItemId>4)
+                {
+                    Console.WriteLine("Invalid menuItemId");
+                    return;
+                }*/
+                var response = client.GetAsync($"Order?menuItemId={menuItemId}");
+                response.Wait();
+                var res = response.Result;
+                if (res.IsSuccessStatusCode)
+                {
+                    var list = res.Content.ReadAsAsync<Cart>();
+                    list.Wait();
+                    var cartItem = list.Result;
+                    Console.WriteLine(cartItem.UserId + "\t" + cartItem.MenuItemId + "\t" + cartItem.MenuItemName);
+                }
+                else
+                {
+                    Console.WriteLine("Item not found");
+                }
+            }
+        }
+    }
+}
